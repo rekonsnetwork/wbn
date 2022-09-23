5be974d751427c3358ce74ac39390a7f9c7fb069
Yahya <yahya6789@gmail.com>
2022-09-23 16:05:41 +0700

    Update manifest.

--- rnet_account/__manifest__.py
+++ rnet_account/__manifest__.py
@@ -3,6 +3,11 @@
     'version': '1.0',
     'depends': ['account'],
     'author': 'RNET',
+    'summary': 'Modifikasi modul accounting',
+    'description': """
+        Modifikasi modul accounting:
+        - Tambah field Ref 2 & 3 di jurnal entries.
+    """,
     'data': [
         'views/account_view.xml',
     ],
