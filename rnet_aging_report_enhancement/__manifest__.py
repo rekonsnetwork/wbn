d3af610b75363f5b13cf9927644487ddab0a2541
Yahya <yahya6789@gmail.com>
2022-10-05 16:37:28 +0700

    Task 21: Laporan Aging.

---
+++ rnet_aging_report_enhancement/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'RNET Aging Report Enhancement',
+    'version': '1.0',
+    'depends': ['bi_partner_ledger_report'],
+    'author': 'Rekons Network',
+    'website': 'http://www.rekons.net/',
+    'summary': 'Enhancement laporan Aged Partner Balance',
+    'description': """
+    """,
+    'data': [
+        'wizard/account_aged_partner_balance.xml'
+    ],
+    'demo': [],
+    'category': 'Accounting',
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+}
